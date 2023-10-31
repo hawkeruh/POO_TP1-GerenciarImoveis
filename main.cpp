231635626b63adf355c66abb6aeaaf2fa189c1c9
hawke <hawkeruh@gmail.com>
2023-10-31 20:48:32 -0300

    falta construtor e sobrecarregar saida

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <iomanip> 
+#include <string>
 #include "objetos.hpp"
 using namespace std;
 
 int main (){
 
+    //Somente UMA coleção polimorfica de objetos no main.
 
+    /*
+    1 - Arquivo com os imoveis possui 16 linhas, cada linha um imovel
+    2 - Inicia programa, ler arquivo para preencher coleção
+    3 - Manipular e transformar coleção com operações:
+        1 - Sobrecarregar operador de saída
+        2 - Dado um proprietario: Função boleana, true se houver algum imovel do proprietario x
+        3 - Dado um valor: Retorna uma coleção com os imoveis de valor igual ou abaixo
+        4 - Dado numero de quartos: Retorna coleção com todos imoveis com numero de quartos igual ou acima
+        5 - 
+    */
 
     return 0;
 }
--- objetos.hpp
+++ objetos.hpp
@@ -1,8 +1,6 @@
+#include "main.cpp" //para bibliotecas
 #ifndef OBJETOS_HPP
 #define OBJEOS_HPP
-
-#include <string>
-using namespace std; 
 
 class Imovel {
     int id, numero, quartos, banheiros;
@@ -36,6 +34,63 @@ public:
     string getBairro() const;
     string getCidade() const;
 
+};
+
+class Casa : public Imovel {
+    int andares; 
+    bool sala_jantar; 
+
+public:
+    Casa(): Imovel();
+    ~Casa();
+
+    void setAndares(int);
+    int getAndares() const;
+
+    void setSalaJantar(bool);
+    bool getSalaJantar(); 
+};
+
+class Apartamento : public Imovel {
+    int andar;
+    float taxa_condominio;
+    bool elevador, sacada;
+
+public:
+    Apartamento(): Imovel();
+    ~Apartamento();
+
+    void setAndar(int);
+    int getAndar() const;
+
+    void setTaxa(float);
+    float getTaxa() const; 
+
+    void setElevador(bool);
+    void setSacada(bool);
+
+    bool getElevador();
+    bool getSacada();
+};
+
+class Chacara : public Imovel {
+    bool salao_festa, salao_jogos, campo_futebol, churrasqueira, piscina; 
+
+public:
+    Chacara(): Imovel();
+    ~Chacara();
+
+    void setSalaoFesta(bool);
+    void setSalaoJogos(bool);
+    void setCampoFutebol(bool);
+    void setChurrasqueira(bool);
+    void setPiscina(bool);
+
+    bool getSalaoFesta();
+    bool getSalaoJogos();
+    bool getCampoFutebol();
+    bool getChurrasqueira();
+    bool getPiscina();
 };
 
 #endif
